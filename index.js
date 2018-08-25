45d3f45d7b9fd70fd35c0bf63fb48d452aa932f4
filippopiconese <filippopiconese@hotmail.it>
2018-08-25 14:57:33 +0200

    test(tested the input which must be a string):

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-module.exports = function parseFlags (input) {
+module.exports = function parseFlags (stringInput) {
+  if (typeof stringInput !== 'string') return new Error('Wrong input, only a string is allowed as input.')
+
   return {}
 }
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,6 +2,8 @@ import test from 'ava'
 
 const flagsParser = require('../index')
 
-test('', t => {
-  t.deepEqual(flagsParser(''), {})
+test('Should throws because the input is not a string', t => {
+  const error = flagsParser(['ciao'])
+
+  t.is(error.message, 'Wrong input, only a string is allowed as input.')
 })
