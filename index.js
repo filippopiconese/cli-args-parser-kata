9962d4ff6ca206c0c9e9bf425e78e28c55e37329
filippopiconese <filippopiconese@hotmail.it>
2018-08-25 16:33:16 +0200

    style(index): extracted the function to get flags from the index

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-module.exports = function parseFlags (stringInput) {
-  if (typeof stringInput !== 'string') return new Error('Wrong input, only a string is allowed as input.')
+module.exports = function parseFlags (str) {
+  if (typeof str !== 'string') return new Error('Wrong input, only a string is allowed as input.')
 
+  return getParsedFlags(str)
+}
+
+function getParsedFlags (str) {
   const parsedFlags = {}
 
-  if (stringInput.includes('--')) {
-    let n = stringInput.indexOf('--') + 2
-    let k = stringInput.indexOf(' ') === -1 ? stringInput.length : stringInput.indexOf(' ')
+  if (str.includes('--')) {
+    let n = str.indexOf('--') + 2
+    let k = str.indexOf(' ') === -1 ? str.length : str.indexOf(' ')
 
-    if (k === stringInput.length) {
-      parsedFlags[stringInput.slice(n, k)] = true
+    if (k === str.length) {
+      parsedFlags[str.slice(n, k)] = true
     } else {
-      parsedFlags[stringInput.slice(n, k)] = parseInt(stringInput.slice(k + 1)) || stringInput.slice(k + 1)
+      parsedFlags[str.slice(n, k)] = parseInt(str.slice(k + 1)) || str.slice(k + 1)
     }
   }
 
